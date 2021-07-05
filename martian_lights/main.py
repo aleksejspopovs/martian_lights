aae8f8ca084acc3defde73ca07410f6c15cbf4c4
Aleksejs Popovs <aleksejs@popovs.lv>
2021-07-05 18:19:04 -0400

    Load configuration from file

--- martian_lights/main.py
+++ martian_lights/main.py
@@ -1,45 +1,68 @@
+import importlib.util
 import logging
 import os
+import sys
 
 from martian_lights.core import MartianLights
 
-def do_the_thing(ml):
-	sensor_id = ml.resource('sensors', 'fake_sensor', {
-		'name': 'fake sensor',
-		'modelid': 'IMSOFAKE',
-		'swversion': '1.0',
-		'type': 'CLIPGenericStatus',
-		'uniqueid': 'fake-00',
-		'manufacturername': 'Aleksejs'
-	})
+def load_config(config_path):
+	spec = importlib.util.spec_from_file_location('config', config_path)
+	module = importlib.util.module_from_spec(spec)
+	spec.loader.exec_module(module)
+	return module
 
-	ml.resource('sensors', 'fake_sensor_2', {
-		'name': f'other sensor, after {sensor_id}',
-		'modelid': 'IMSOFAKE',
-		'swversion': '1.0',
-		'type': 'CLIPGenericStatus',
-		'uniqueid': 'fake-01',
-		'manufacturername': 'Aleksejs'
-	})
+def read_config_value(config, name, env_var):
+	if env_var in os.environ:
+		return os.environ[env_var]
+	if hasattr(config, name):
+		return getattr(config, name)
+
+	logging.error(
+		(
+			'Configuration value %s not found, please supply by setting it in '
+			'the config file or in the environment variable %s.'
+		),
+		name,
+		env_var,
+	)
+	sys.exit(1)
+
+def initialize_ml(config):
+	state_path = read_config_value(config, 'state_path', 'HUE_STATE_PATH')
+	bridge_addr = read_config_value(config, 'bridge_addr', 'HUE_BRIDGE_ADDR')
+	bridge_username = read_config_value(config, 'bridge_username', 'HUE_BRIDGE_USERNAME')
+	return MartianLights(state_path, bridge_addr, bridge_username)
 
 def main():
+	if len(sys.argv) != 2:
+		print('USAGE:')
+		print(f'{sys.argv[0]} config.py')
+		sys.exit(1)
+
 	logging.basicConfig(
 		level=logging.INFO,
 		format='[%(levelname)s] %(message)s'
 	)
 
-	ml = MartianLights('state.json', os.getenv('HUE_BRIDGE_ADDR'), os.getenv('HUE_BRIDGE_USERNAME'))
+	config_path = sys.argv[1]
+	config = load_config(config_path)
+	ml = initialize_ml(config)
+
 	ran_cleanly = True
 
 	try:
-		do_the_thing(ml)
+		config.run(ml)
 	except Exception as error:
 		ran_cleanly = False
 		logging.error(repr(error))
-	finally:
-		if ran_cleanly:
-			ml.delete_ghost_resources()
-		ml.save_state()
+
+	if ran_cleanly:
+		ml.delete_ghost_resources()
+
+	ml.save_state()
+
+	if not ran_cleanly:
+		sys.exit(1)
 
 if __name__ == '__main__':
 	main()
