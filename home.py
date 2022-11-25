80033e60fa8d67161aca34530f1d976d85873656
Aleksejs Popovs <aleksejs@popovs.lv>
2022-11-25 15:10:54 -0500

    Update home

--- home.py
+++ home.py
@@ -1,3 +1,4 @@
+from martian_lights.behaviors.group_toggle import group_toggle
 from martian_lights.behaviors.rotary_brightness import rotary_brightness
 from martian_lights.behaviors.time_based_scene_cycling import time_based_scene_cycling
 from martian_lights.behaviors.two_subgroup_cycling import two_subgroup_cycling
@@ -26,61 +27,84 @@ def make_bathroom(ml):
 		off_conditions=[switch.top().short_release()],
 		cycle_conditions=[switch.top().long_release()],
 		scenes=[
-			('T07:00:00/T12:00:00', 'wjg08c8U6YpmGgp'), # Energize
-			('T12:00:00/T19:00:00', 'ZdoNb40lFPAs19y'), # Bright
-			('T19:00:00/T22:00:00', '49N5awEOEXcEm73'), # Dimmed
-			('T22:00:00/T07:00:00', 'tBFcir3B4akYah8'), # Nightlight
+			('T07:00:00/T12:00:00', '6vIegaD7zCterRy'), # Energize
+			('T12:00:00/T19:00:00', 'fMvcJgY4Vv0NaGu'), # Bright
+			('T19:00:00/T22:00:00', 'OA80foEuTaVeVcq'), # Dimmed
+			('T22:00:00/T07:00:00', '10Xnn5pUoEC34dv'), # Nightlight
 		],
 		display_name='Bathroom scenes',
 	)
 
 
 def make_living_room(ml):
-	group_id = 3
-	switches = [
-		ZLLSwitch(42), # couch
-		ZLLSwitch(46), # dining area
+	tap_switches = [ZLLSwitch(57), ZLLSwitch(63)]
+	rotaries = [56, 62]
+	couch_switch = ZLLSwitch(46)
+
+	foh_group_id = 7 # front of house
+	schedule = [
+		('T07:00:00/T12:00:00', 'iJR082ijBR0ejKA'), # Energize
+		('T12:00:00/T19:00:00', 'UiMXRDvpXuAtd7e'), # Bright
+		('T19:00:00/T07:00:00', 'lf19gLwaFlFz02D'), # Dimmed
 	]
 
-	scene_cycle_state_id = time_based_scene_cycling(
-		ml.namespace('scenes'),
-		group_id=group_id,
-		on_conditions=[s.top().initial_press() for s in switches],
-		off_conditions=[s.top().initial_press() for s in switches],
-		cycle_conditions=[s.bottom().long_release() for s in switches],
-		scenes=[
-			('T07:00:00/T12:00:00', '6VE4rvAva5a3YLd'), # Energize
-			('T12:00:00/T19:00:00', 'n0AH1Tc85L8Rc-3'), # Bright
-			('T19:00:00/T07:00:00', 'wVOwX6LGwOyQIiE'), # Dimmed
-		],
+	time_based_scene_cycling(
+		ml.namespace(f'scenes'),
+		group_id=foh_group_id,
+		on_conditions=[s.one().short_release() for s in tap_switches] + [couch_switch.top().short_release()],
+		off_conditions=[s.three().short_release() for s in tap_switches] + [couch_switch.top().short_release()],
+		cycle_conditions=[s.two().short_release() for s in tap_switches],
+		scenes=schedule,
 		display_name='Living room scenes',
 	)
 
-	for switch in switches:
-		zll_switch_brightness(
-			ml.namespace(f'bri_{switch.sensor_id}'),
-			switch=switch,
-			group_id=group_id,
+	for rotary in rotaries:
+		rotary_brightness(
+			ml.namespace(f'bri_{rotary}'),
+			rotary_sensor_id=rotary,
+			group_id=foh_group_id,
 			display_name='Living room brightness',
+			slow_rotation_turns_lights_to=None,
+			fast_rotation_turns_lights_to=None,
 		)
 
-	two_subgroup_cycling(
-		ml.namespace('subgroup'),
-		# these groups already exist
-		subgroups=[
-			6, # LR Dining
-			5, # LR Couch
-		],
-		advance_conditions=[s.bottom().short_release() for s in switches],
+	subgroups = [
+		(10, ZLLSwitchButtonType.ONE, ['aLvpEr9eDDkP47b', 'LmVi205gkh9aG4f', '83IINrA9VED9SUd']), # hallway
+		(3, ZLLSwitchButtonType.TWO, ['6VE4rvAva5a3YLd', 'n0AH1Tc85L8Rc-3', 'wVOwX6LGwOyQIiE']), # living room
+		(9, ZLLSwitchButtonType.THREE, ['0Ind8LjEEyjcxaM', 'Ni2aDscg7GICwjj', 'B-JjJkUEuKiZboy']), # kitchen
+		(6, ZLLSwitchButtonType.FOUR, ['0p3DAJiqlukpdbL', 'P3ATIR7FnoRD71p', 'gVhhGq4JbKbBNcN']), # alcove
+	]
+
+	for subgroup_id, button_type, _ in subgroups:
+		group_toggle(
+			ml.namespace(f'toggle_{subgroup_id}'),
+			group_id=subgroup_id,
+			conditions=[s.button(button_type).long_release() for s in tap_switches],
+		)
+
+	# couch switch
+	lr_group_id = 3
+
+	zll_switch_brightness(
+		ml.namespace(f'bri_couch'),
+		switch=couch_switch,
+		group_id=lr_group_id,
+		display_name='Couch brightness',
+	)
+
+	group_toggle(
+		ml.namespace('toggle_couch'),
+		group_id=lr_group_id,
+		conditions=[couch_switch.bottom().short_release()],
 	)
 
 
-def make_bedroom(ml):
+def make_bedroom(ml, scenes):
 	group_id = 4
 	wall_switch = ZLLSwitch(2)
 	
-	puck_id_rotary = 53
-	puck_button = ZLLSwitch(54)
+	puck_id_rotary = 67
+	puck_button = ZLLSwitch(68)
 	
 	time_based_scene_cycling(
 		ml.namespace('scenes'),
@@ -89,10 +113,10 @@ def make_bedroom(ml):
 		off_conditions=[wall_switch.bottom().initial_press(), puck_button.top().initial_press()],
 		cycle_conditions=[wall_switch.top().initial_press(), puck_button.top().long_release()],
 		scenes=[
-			('T07:00:00/T12:00:00', '4F0yl9YDbgf29Aa'), # Energize
-			('T12:00:00/T19:00:00', '5wmuhH4G9GFJMyV'), # Relax
-			('T19:00:00/T21:00:00', 'YKF5lteVdMkcfJN'), # Dimmed
-			('T21:00:00/T07:00:00', 'rT8HfqWAYtT50U5'), # Nightlight
+			('T07:00:00/T12:00:00', scenes[group_id]['Energize']),
+			('T12:00:00/T19:00:00', scenes[group_id]['Relax']),
+			('T19:00:00/T21:00:00', scenes[group_id]['Dimmed']),
+			('T21:00:00/T07:00:00', scenes[group_id]['Nightlight']),
 		],
 		display_name='Bedroom scenes',
 	)
@@ -116,53 +140,65 @@ def make_bedroom(ml):
 
 def make_office(ml):
 	group_id = 1
-	switch = ZLLSwitch(6)
+	wall_switch = ZLLSwitch(6)
+	desk_switch = ZLLSwitch(42)
+	switches = [wall_switch, desk_switch]
 
 	scene_cycle_state_id = time_based_scene_cycling(
 		ml.namespace('scenes'),
 		group_id=group_id,
-		on_conditions=[switch.top().initial_press()],
-		off_conditions=[switch.top().initial_press()],
-		cycle_conditions=[switch.bottom().long_release()],
+		on_conditions=[s.top().initial_press() for s in switches],
+		off_conditions=[s.top().initial_press() for s in switches],
+		cycle_conditions=[s.bottom().long_release() for s in switches],
 		scenes=[
-			('T07:00:00/T10:00:00', 'aBmkEihL80mswg5'), # Energize
-			('T10:00:00/T17:00:00', '1zpkcVBUVnUMTmi'), # Concentrate
-			('T17:00:00/T20:00:00', 'GhRn89mSmwR2f-m'), # Read
-			('T20:00:00/T23:00:00', 'c90XW-mrnIhJ9z-'), # Relax
-			('T23:00:00/T07:00:00', 'RL747XrAtHPQvPh'), # Nightlight
+			('T07:00:00/T10:00:00', 'NppUTl2AYdRNLQF'), # Energize
+			('T10:00:00/T17:00:00', '4hNMi8DCp2MBYJN'), # Concentrate
+			('T17:00:00/T20:00:00', 'LZvd86NAkzlbhEN'), # Read
+			('T20:00:00/T23:00:00', 'lZadNORbla8JrLy'), # Relax
+			('T23:00:00/T07:00:00', '2Ne8xZRiXKl00FK'), # Nightlight
 		],
 		display_name='Office scenes',
 	)
 
-	zll_switch_brightness(
-		ml.namespace(f'bri'),
-		switch=switch,
-		group_id=group_id,
-		display_name='Office brightness',
-	)
+	for i, switch in enumerate(switches):
+		zll_switch_brightness(
+			ml.namespace(f'bri_{i}'),
+			switch=switch,
+			group_id=group_id,
+			display_name='Office brightness',
+		)
 
 	# these groups are created with type: LightGroup, so they do not appear on
 	# the mobile app
 	overhead_group = ml.resource(
 		'groups',
 		'overhead_group',
-		{'name': 'OF Overhead', 'lights': ['4', '5', '6']},
+		{'name': 'OF Overhead', 'lights': ['29', '30', '31']},
 	)
-	table_lamp_group = ml.resource(
+	desk_lamp_group = ml.resource(
 		'groups',
-		'table_lamp_group',
-		{'name': 'OF Table', 'lights': ['11']},
+		'desk_lamp_group',
+		{'name': 'OF Desk', 'lights': ['10']},
 	)
 
 	two_subgroup_cycling(
 		ml.namespace('subgroup'),
-		subgroups=[overhead_group, table_lamp_group],
-		advance_conditions=[switch.bottom().short_release()],
+		subgroups=[overhead_group, desk_lamp_group],
+		advance_conditions=[s.bottom().short_release() for s in switches],
 	)
 
 
 def run(ml):
+	scenes = ml.read_resource('scenes', None)
+	scenes_by_group = {}
+	for scene_id, scene in scenes.items():
+		group = int(scene['group'])
+		if group not in scenes_by_group:
+			scenes_by_group[group] = {}
+		scenes_by_group[group][scene['name']] = scene_id
+
 	make_bathroom(ml.namespace('bathroom'))
 	make_living_room(ml.namespace('living_room'))
-	make_bedroom(ml.namespace('bedroom'))
+	make_bedroom(ml.namespace('bedroom'), scenes_by_group)
 	make_office(ml.namespace('office'))
+	pass
